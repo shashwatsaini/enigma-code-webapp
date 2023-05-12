1d4175a541b00a5756ba7459c4c0349abc5c3a64
Anuj Dwivedi <anujd0009@gmail.com>
2023-05-12 12:43:22 +0530

    fully func ---v -0.0.01

--- app.py
+++ app.py
@@ -5,33 +5,42 @@ from flask_login import LoginManager, UserMixin, login_required, current_user
 from flask import session
 from uuid import uuid4
 
-
 app = Flask(__name__)
 app.secret_key = '12345678'
-enigma_code = "ABCD123456"
-
+enigma_code = "MAYNS38764"
 
 print("---------------------- start login management --------------------")
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+double_l=0
 class User(UserMixin):
     def __init__(self, team_number, code):
         self.team_number = team_number
         self.code = code
         self.session_id = None
+
     def get_id(self):
         return str(self.team_number)
+
     def __repr__(self):
         return f'<User {self.team_number}>'
+
     def login(self):
+        global double_l
+        if self.session_id is not None:
+            double_l=1
         self.session_id = str(uuid4())
         session['user_id'] = self.get_id()
         session['session_id'] = self.session_id
+
     def logout(self):
         self.session_id = None
         session.pop('user_id', None)
         session.pop('session_id', None)
+
+
 
 users = {
     'Team_01': User('Team_01', '1325609313'),
@@ -91,23 +100,30 @@ users = {
     'Admin_3': User('Admin_3', '1054178774'),
     'Admin_4': User('Admin_4', '7017760686'),
    }
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return users.get(user_id)
+
+
 print("-------------------------  end login management  -----------------------------------")
 
 print("-------------------------- START RouTe MapPing --------------------------------------")
 
-
 print("--------------------------- BASIC UP-FRONT START ------------------------------------")
+
+
 @app.route('/')
 def wait():
-    seconds_to_wait = 21 # replace with desired number of seconds to wait
+    seconds_to_wait = 21  # replace with desired number of seconds to wait
     return render_template('wait.html', seconds=seconds_to_wait)
+
 
 # define login route and function
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    global double_l
     if request.method == 'POST':
         print("_______  ENTER POST CAPTURE  ______")
         team_number = request.form['team_number']
@@ -121,10 +137,7 @@ def login():
         if user is not None and user.code == code:
             print("--- USER VALID IN DATABASE ---")
             # check if user is already logged in
-            if user.session_id and session.get('session_id') != user.session_id:
-                print('DOUBLE DEVICE LOGIN -- ATTEMPT')
-                flash('Another device is already logged in with this account.')
-                return redirect(url_for('login'))
+
 
             # login the user
             user.login()
@@ -138,26 +151,28 @@ def login():
 
     return render_template('login.html')
 
+
 print("-------------------------- BASIC UP-FRONT END --------------------------------------")
 
-
-
-
-
 print("--------------------------  HOME AND MENU   --------------------------------------")
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenges')
 def challenges():
     return render_template('home.html')
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
 @app.route('/company-link')
 def company_link():
     return redirect('https://linktr.ee/turingthoughts')
 
+
 print("--------------------------    MENU END    --------------------------------------")
-
-
 
 print("--------------------------   CHALLENGES   --------------------------------------")
 print("                       ONE 1. ENCRYPTION CHALLENGE                              ")
@@ -166,6 +181,7 @@ print("                       ONE 1. ENCRYPTION CHALLENGE                       
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge1')
 def challenge1():
     return render_template('challenge1.html')
+
 
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge1-validator', methods=['POST'])
 def challenge1_validator():
@@ -177,12 +193,15 @@ def challenge1_validator():
         return render_template('challenge1.html', number=None, result='Try again!')
 
 
-
 print("-------------------------------------------------------------------------------")
 print("                        TWO 2. TECH-BASICS CHALLENGE                                 ")
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge2')
 def challenge2():
     return render_template('challenge2.html')
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge2-validator', methods=['POST'])
 def challenge2_validator():
     riddle1_answer = request.form.get('riddle1')
@@ -196,12 +215,15 @@ def challenge2_validator():
         return render_template('challenge2.html', result='TRY AGAIN')
 
 
-
 print("-------------------------------------------------------------------------------")
 print("                        THREE 3. RIDDLE CHALLENGE                                 ")
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge3')
 def challenge3():
     return render_template('challenge3.html')
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge3-validator', methods=['POST'])
 def challenge3_validator():
     riddle1_answer = request.form.get('riddle1')
@@ -217,9 +239,12 @@ def challenge3_validator():
 
 print("-------------------------------------------------------------------------------")
 print("                        FOUR 4. CHESS QUEEN CHALLENGE                            ")
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge4')
 def challenge4():
     return render_template('challenge4.html')
+
 
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge4-validator', methods=['POST'])
 def challenge4_validator():
@@ -233,6 +258,7 @@ def challenge4_validator():
     else:
         return '', 400
 
+
 def is_valid_chessboard(chessboard):
     for row in range(8):
         queens_in_row = sum(chessboard[row])
@@ -240,10 +266,10 @@ def is_valid_chessboard(chessboard):
             return False
         elif queens_in_row == 1:
             col = chessboard[row].index(1)
-            for i in range(col-1, -1, -1): # check left side of the row
+            for i in range(col - 1, -1, -1):  # check left side of the row
                 if chessboard[row][i] == 1:
                     return False
-            for i in range(col+1, 8): # check right side of the row
+            for i in range(col + 1, 8):  # check right side of the row
                 if chessboard[row][i] == 1:
                     return False
             for col in range(8):
@@ -276,18 +302,22 @@ def is_valid_chessboard(chessboard):
                                 return False
 
             return True
+
+
 def calculate_number(chessboard):
     columns = [chessboard[row].index(1) for row in range(8)]
-    return sum([columns[i] * (10 ** (7-i)) for i in range(8)])
-
-
+    return sum([columns[i] * (10 ** (7 - i)) for i in range(8)])
 
 
 print("-------------------------------------------------------------------------------")
 print("                         FIVE 5. IMAGE CHALLENGE                               ")
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge5')
 def challenge5():
     return render_template('challenge5.html')
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge5-validator', methods=['POST'])
 def challenge5_validator():
     answer1 = request.form.get('q1')
@@ -295,17 +325,19 @@ def challenge5_validator():
     number = enigma_code[4]
 
     if answer1 == 'b' and answer2 == 'c':
-        return render_template('challenge5.html',number=number, result='CORRECT')
+        return render_template('challenge5.html', number=number, result='CORRECT')
     else:
-        return render_template('challenge5.html',number=None, result='TRY AGAIN')
-
+        return render_template('challenge5.html', number=None, result='TRY AGAIN')
 
 
 print("-------------------------------------------------------------------------------")
 print("                          SIX 6. UNSCRAMBLE RAMBLE                             ")
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge6')
 def challenge6():
     return render_template('challenge6.html')
+
 
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge6-validator', methods=['POST'])
 def challenge6_validator():
@@ -314,23 +346,25 @@ def challenge6_validator():
     number = enigma_code[5]
 
     if question1_answer.strip().lower() == 'parachute' and question2_answer.strip().lower() == 'anniversary':
-        return render_template('challenge6.html',number=number, result='SUCCESSFUL !')
+        return render_template('challenge6.html', number=number, result='SUCCESSFUL !')
     else:
-        return render_template('challenge6.html',number=None,  result='TRY AGAIN')
-
+        return render_template('challenge6.html', number=None, result='TRY AGAIN')
 
 
 print("-------------------------------------------------------------------------------")
 print("                           SEVEN 7 . QUATER - 1                                 ")
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge7')
 def challenge7():
     global answer
     # Generate 4 random numbers between 1 and 10
-    A, B, C, D = [7,6,7,6]
+    A, B, C, D = [7, 6, 7, 6]
     # Compute the answer based on the formula
     answer = int(math.sqrt(A - B) + math.pow(C, 3) - math.pow(C - D, 2) + 2)
     print(answer)
     return render_template('challenge7.html', number=None)
+
 
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge7-validator', methods=['POST'])
 def challenge7_validator():
@@ -338,10 +372,10 @@ def challenge7_validator():
     print("-- ENTER VALIDATOR --")
     num = request.form['number']
     print(num)
-    num = int(num)
+    num = float(num)
     number = enigma_code[6]
     print(number)
-    if answer == num:
+    if num in (43,44,45):
         print("ENTER IF")
         return render_template('challenge7.html', number=number, result='Congratulations!')
     else:
@@ -349,26 +383,29 @@ def challenge7_validator():
         return render_template('challenge7.html', number=None, result='Try again!')
 
 
-
 print("-------------------------------------------------------------------------------")
 print("                          EIGHT 8  . QUATER - 2                                 ")
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge8')
 def challenge8():
     global answer
-    A, B, C, D = [7,6,7,6]
+    A, B, C, D = [7, 6, 7, 6]
     answer = int(math.sqrt(A - B) + math.pow(C, 3) - math.pow(C - D, 2) + 2)
     print(answer)
     return render_template('challenge8.html', number=None)
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge8-validator', methods=['POST'])
 def challenge8_validator():
     global answer
     print("-- ENTER VALIDATOR --")
     num = request.form['number']
     print(num)
-    num = int(num)
+    num = float(num)
     number = enigma_code[7]
     print(number)
-    if answer == num:
+    if num in (11,12,13):
         print("ENTER IF")
         return render_template('challenge8.html', number=number, result='Congratulations!')
     else:
@@ -378,23 +415,27 @@ def challenge8_validator():
 
 print("-------------------------------------------------------------------------------")
 print("                           NINE 9 . QUATER - 3                                 ")
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge9')
 def challenge9():
     global answer
-    A, B, C, D = [7,6,7,6]
+    A, B, C, D = [7, 6, 7, 6]
     answer = int(math.sqrt(A - B) + math.pow(C, 3) - math.pow(C - D, 2) + 2)
     print(answer)
     return render_template('challenge9.html', number=None)
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge9-validator', methods=['POST'])
 def challenge9_validator():
     global answer
     print("-- ENTER VALIDATOR --")
     num = request.form['number']
     print(num)
-    num = int(num)
+    num = float(num)
     number = enigma_code[8]
     print(number)
-    if answer == num:
+    if num in (8,9,10):
         print("ENTER IF")
         return render_template('challenge9.html', number=number, result='Congratulations!')
     else:
@@ -404,23 +445,28 @@ def challenge9_validator():
 
 print("-------------------------------------------------------------------------------")
 print("                            TEN 10 . QUATER - 4                                 ")
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge10')
 def challenge10():
     global answer
-    A, B, C, D = [7,6,7,6]
+    A, B, C, D = [7, 6, 7, 6]
     answer = int(math.sqrt(A - B) + math.pow(C, 3) - math.pow(C - D, 2) + 2)
     print(answer)
     return render_template('challenge10.html', number=None)
+
+
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge10-validator', methods=['POST'])
 def challenge10_validator():
     global answer
     print("-- ENTER VALIDATOR --")
     num = request.form['number']
     print(num)
-    num = int(num)
+    num = float(num)
     number = enigma_code[9]
     print(number)
-    if answer == num:
+
+    if num in (57.2723, 58, 59, 57, 56):
         print("ENTER IF")
         return render_template('challenge10.html', number=number, result='Congratulations!')
     else:
@@ -428,26 +474,27 @@ def challenge10_validator():
         return render_template('challenge10.html', number=None, result='Try again!')
 
 
-
 print("-------------------------------------------------------------------------------")
 print("                             CHALLENGES END                                    ")
 print("-------------------------------------------------------------------------------")
 
-
-
 print("--------------------------------  eNIGMa Validation  -----------------------------")
+
+
 @app.route('/enigma-validator-render')
 def enigma_validator_render():
     return render_template('enigma_validator.html')
+
+
 @app.route('/enigma-validator', methods=['POST'])
 def validate_enigma_code():
     global enigma_code
     code = request.json['code']
     matrix = [1 if c == enigma_code[i] else 0 for i, c in enumerate(code)]
     return jsonify(matrix)
+
+
 print("---------------------------------------------------------------------------------")
-
-
 
 print("----------------------------  end route mapping  ----------------------------------")
 
