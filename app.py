ea859c95ce320096df6846c8bf3c4155c8384202
Shashwat Saini <shashwatsaini290@gmail.com>
2023-07-11 09:36:18 +0530

    Enhanced Login Features
    1. Replaced Users' dictionary with sqlite framework, and moved the dictionary into a text file for easy access.
    2. Improved login features- Added functions for is_authenticated, implemented usage of current_user, and created a working logout function as well.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import math
+from time import time
 
-from flask import Flask, render_template, redirect, url_for, flash, request, jsonify
-from flask_login import LoginManager, UserMixin, login_required, current_user
-from flask import session
+from flask import Flask, render_template, redirect, url_for, flash, request, jsonify, after_this_request, make_response
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
+from flask_sqlalchemy import SQLAlchemy
 from uuid import uuid4
 
 app = Flask(__name__)
@@ -14,105 +15,58 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-double_l=0
-class User(UserMixin):
-    def __init__(self, team_number, code):
-        self.team_number = team_number
-        self.code = code
-        self.session_id = None
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+
+class User(UserMixin, db.Model):
+    team_number = db.Column(db.String(20), primary_key=True)
+    code = db.Column(db.String(20), unique=True)
+    session_id = db.Column(db.String(36), unique=False, nullable=True, default=None)
 
     def get_id(self):
         return str(self.team_number)
+    
+    def is_authenticated(self):
+        if self.session_id is None:
+            return False
+        return True
 
     def __repr__(self):
         return f'<User {self.team_number}>'
-
+    
     def login(self):
-        global double_l
-        if self.session_id is not None:
-            double_l=1
         self.session_id = str(uuid4())
-        session['user_id'] = self.get_id()
-        session['session_id'] = self.session_id
-
+        db.session.commit()
+    
     def logout(self):
         self.session_id = None
-        session.pop('user_id', None)
-        session.pop('session_id', None)
+        db.session.commit()
 
+with open('users.txt','r') as file:
+    users = file.readlines()
+    users = {user.split(',')[0]: user.split(',')[1].replace('\n','') for user in users}
+    print('---------------------- users initialized ----------------------')
+    print(users)
 
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
-users = {
-    'Team_01': User('Team_01', '1325609313'),
-    'Team_01_e': User('Team_01_e', '1325609313'),
-    'Team_02': User('Team_02', '2289067155'),
-    'Team_02_e': User('Team_02_e', '2289067155'),
-    'Team_03': User('Team_03', '3428589256'),
-    'Team_03_e': User('Team_03_e', '3428589256'),
-    'Team_04': User('Team_04', '5481345038'),
-    'Team_04_e': User('Team_04_e', '5481345038'),
-    'Team_05': User('Team_05', '5902394953'),
-    'Team_05_e': User('Team_05_e', '5902394953'),
-    'Team_06': User('Team_06', '4942522877'),
-    'Team_06_e': User('Team_06_e', '4942522877'),
-    'Team_07': User('Team_07', '3466675718'),
-    'Team_07_e': User('Team_07_e', '3466675718'),
-    'Team_08': User('Team_08', '6002842080'),
-    'Team_08_e': User('Team_08_e', '6002842080'),
-    'Team_09': User('Team_09', '1845326422'),
-    'Team_09_e': User('Team_09_e', '1845326422'),
-    'Team_10': User('Team_10', '2354781676'),
-    'Team_10_e': User('Team_10_e', '2354781676'),
-    'Team_11': User('Team_11', '3484094425'),
-    'Team_11_e': User('Team_11_e', '3484094425'),
-    'Team_12': User('Team_12', '1580755304'),
-    'Team_12_e': User('Team_12_e', '1580755304'),
-    'Team_13': User('Team_13', '4164845592'),
-    'Team_13_e': User('Team_13_e', '4164845592'),
-    'Team_14': User('Team_14', '8694537408'),
-    'Team_14_e': User('Team_14_e', '8694537408'),
-    'Team_15': User('Team_15', '1686370263'),
-    'Team_15_e': User('Team_15_e', '1686370263'),
-    'Team_16': User('Team_16', '3251818970'),
-    'Team_16_e': User('Team_16_e', '3251818970'),
-    'Team_17': User('Team_17', '9860061699'),
-    'Team_17_e': User('Team_17_e', '9860061699'),
-    'Team_18': User('Team_18', '8256083990'),
-    'Team_18_e': User('Team_18_e', '8256083990'),
-    'Team_19': User('Team_19', '4035639024'),
-    'Team_19_e': User('Team_19_e', '4035639024'),
-    'Team_20': User('Team_20', '8326083990'),
-    'Team_20_e': User('Team_20_e', '8326083990'),
-    'Team_21': User('Team_21', '4035639024'),
-    'Team_21_e': User('Team_21_e', '4035639024'),
-    'Team_22': User('Team_22', '7326083990'),
-    'Team_22_e': User('Team_22_e', '7326083990'),
-    'Team_23': User('Team_23', '2035639024'),
-    'Team_23_e': User('Team_23_e', '2035639024'),
-    'Team_24': User('Team_24', '7326086990'),
-    'Team_24_e': User('Team_24_e', '7326086990'),
-    'Team_25': User('Team_25', '2035676024'),
-    'Team_25_e': User('Team_25_e', '2035676024'),
-    'Team_26': User('Team_26', '2035676024'),
-    'Team_26_e': User('Team_26_e', '2035676024'),
-    'Admin_1': User('Admin_1', '9019458974'),
-    'Admin_2': User('Admin_2', '9025021947'),
-    'Admin_3': User('Admin_3', '1054178774'),
-    'Admin_4': User('Admin_4', '7017760686'),
-   }
-
+    for team_number, code in users.items():
+        user = User(team_number=team_number, code=code)
+        db.session.add(user)
+        db.session.commit()
 
 @login_manager.user_loader
 def load_user(user_id):
-    return users.get(user_id)
-
+    return User.query.get(user_id)
 
 print("-------------------------  end login management  -----------------------------------")
 
 print("-------------------------- START RouTe MapPing --------------------------------------")
 
 print("--------------------------- BASIC UP-FRONT START ------------------------------------")
-
 
 @app.route('/')
 def wait():
@@ -123,7 +77,6 @@ def wait():
 # define login route and function
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    global double_l
     if request.method == 'POST':
         print("_______  ENTER POST CAPTURE  ______")
         team_number = request.form['team_number']
@@ -132,15 +85,14 @@ def login():
         print(code)
 
         # validate user credentials
-        user = users.get(team_number)
-
+        user = User.query.get(team_number)
+                              
         if user is not None and user.code == code:
             print("--- USER VALID IN DATABASE ---")
-            # check if user is already logged in
-
 
             # login the user
             user.login()
+            login_user(user)
             print("LOG-IN Successful")
             flash('Log in with given password.')
             return redirect(url_for('challenges'))
@@ -159,7 +111,9 @@ print("--------------------------  HOME AND MENU   -----------------------------
 
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenges')
 def challenges():
-    return render_template('home.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    return render_template('home.html', cache_bust=time())
 
 
 @app.route('/about')
@@ -444,7 +398,7 @@ def challenge9_validator():
 
 
 print("-------------------------------------------------------------------------------")
-print("                            TEN 10 . QUATER - 4                                 ")
+print("         cx                   TEN 10 . QUATER - 4                                 ")
 
 
 @app.route('/i5i5rsuopeyr10cov4ovuev06cb1sxchallenge10')
@@ -499,12 +453,18 @@ print("-------------------------------------------------------------------------
 print("----------------------------  end route mapping  ----------------------------------")
 
 
-@app.route('/logout')
+@app.route('/logout/')
 @login_required
 def logout():
-    current_user.logout()
-    flash('Logged out successfully.')
-    return redirect(url_for('login'))
+    user = current_user
+    print(user.is_authenticated)
+    if user.is_authenticated:
+        user.logout()
+        logout_user()
+        flash('Logged out successfully.')
+    response = make_response(redirect(url_for('login')))
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    return response
 
 
 if __name__ == '__main__':
